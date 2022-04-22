774f2dae6ca4ab9ae23c10043ae192920ebd067f
guillermo.leal <guillermo01.galp@gmail.com>
2022-04-21 21:27:36 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server on port " + process.env.PORT);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
